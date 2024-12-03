d4787a6debdd67ad4add58f38b017d8381d0f159
arsalan <arsalan@gmail.come>
2024-12-03 20:37:49 +0500

    link add

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+<ul className=' flex justify-center items-center gap-5'>
+    <li className=' line-none'> <Link to={'/form'}>React Form</Link> </li>
+    <li><Link to={'/todo'}>Todo</Link></li> </ul>
+    </div>
   )
 }
 
